de6ab9a2919ec3c41e6e6c479916da0e608fcd08
Stefano Morgia <ste.morgia@gmail.com>
2022-09-26 15:55:20 +0200

    esercizio base completato

--- laravel-primi-passi/routes/web.php
+++ laravel-primi-passi/routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    
+    $numeroCasuale = rand(0, 100);
+
+    return view('home', ['numeroCasualeViews' => $numeroCasuale]);
+    
 });
